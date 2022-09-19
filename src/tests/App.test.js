6081cfd7273ef349f74ea414024690c027fc68ea
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-09-19 18:50:08 -0300

    requisito um

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,19 +5,6 @@ import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testando todos os links da barra de navegação', () => {
-  test('Testandoos meus links', () => {
-    renderWithRouter(<App />);
-
-    const homeLink = screen.getByRole('link', { name: /home/i });
-    expect(homeLink).toBeInTheDocument(); // uso do Testing Playground
-
-    const aboutLink = screen.getByRole('link', { name: /about/i });
-    expect(aboutLink).toBeInTheDocument();
-
-    const favoritesLink = screen.getByRole('link', { name: /favorite pokémons/i });
-    expect(favoritesLink).toBeInTheDocument();
-  });
-
   test('Testando redireciona o Link Home', () => {
     const { history } = renderWithRouter(<App />);
     const homeLink = screen.getByRole('link', { name: /home/i });
