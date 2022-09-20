4c3be56be0e2b69f9f327b2e33154d53c6b78f5d
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-09-20 00:22:45 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Teste o componente favorites pokemon ', () => {
+  renderWithRouter(<FavoritePokemons />);
+  const elementFavirites = screen.getByText(/no favorite pokemon found/i);
+  expect(elementFavirites).toBeInTheDocument();
+});
