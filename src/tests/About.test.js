bd17387eff2199f08cc8ef49d5024a0aadf4ed4f
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-09-20 00:03:01 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+test('Testanto minhas tags e imagem no About.js', () => {
+  renderWithRouter(<About />);
+  const testandoTag = screen.getByRole('heading', { name: /about pokédex/i });
+  expect(testandoTag).toBeInTheDocument();
+
+  const testLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  const testImage = screen.getByRole('img', { name: /Pokédex/i });
+
+  expect(testImage).toHaveAttribute('src', testLink);
+});
